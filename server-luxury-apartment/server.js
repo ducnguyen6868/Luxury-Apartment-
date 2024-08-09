ab33524a12725085859d16e73337b71125fb7a42
tranminhqiang <tranhoangminhquang12022002@gmail.com>
2024-08-09 10:06:26 +0700

    Add,delete and fix feature

--- server-luxury-apartment/server.js
+++ server-luxury-apartment/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const Apartment = require('./model/apartmentModel');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
-const uri = 'mongodb://localhost:27017/luxury-apartment';
+const uri = 'mongodb://127.0.0.1:27017/apartmentDB';
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 const connection = mongoose.connection;
 connection.once('open', () => {
@@ -62,9 +63,9 @@ const ApartmentSchema = new mongoose.Schema({
   contactInfo: ContactInfoSchema,
 });
 
-const Apartment = mongoose.model('Apartment', ApartmentSchema);
+// const Apartment = mongoose.model('Apartment', ApartmentSchema);
 
-module.exports = Apartment;
+// module.exports = Apartment;
 
 const EmployeeSchema = mongoose.Schema(
   {
@@ -84,6 +85,87 @@ app.get('/', async (req, res) => {
     res.status(400).json('Error: ' + err);
   }
 });
+app.get('/product', async (req, res) => {
+  try {
+    const apartments = await Apartment.find();
+    res.json(apartments);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+app.get('/product/:code', async (req, res) => {
+  try {
+    const { code } = req.params;
+    const apartment = await Apartment.findOne({ code });
+    
+    if (apartment) {
+      res.json(apartment);
+    } else {
+      res.status(404).json({ message: 'Apartment not found' });
+    }
+  } catch (err) {
+    res.status(400).json({ error: 'Error: ' + err });
+  }
+});
+
+app.post('/product/:code', async (req, res) => {
+  try {
+    const { code } = req.params;
+    const apartment = await Apartment.deleteOne({code})
+    if (apartment) {
+      res.json(apartment);
+    } else {
+      res.status(404).json({ message: 'Apartment not found' });
+    }
+  } catch (err) {
+    res.status(400).json({ error: 'Error: ' + err });
+  }
+});
+
+app.post('/fix-apartment/:code', async (req, res) => {
+  try {
+    const { type, price, name, description } = req.body;
+    const { code } = req.params;
+    const apartment = await Apartment.deleteOne({code})
+    if (apartment) {
+      res.json(apartment);
+    } else {
+      res.status(404).json({ message: 'Apartment not found' });
+    }
+    const newApartment = new Apartment({
+      type,
+      price,
+      name,
+      code,
+      description
+    });
+
+    await newApartment.save();
+    res.status(201).json({ message: 'Apartment added successfully!' });
+  } catch (err) {
+    res.status(400).json({ error: 'Error: ' + err });
+  }
+});
+
+app.post('/add-apartment', async (req, res) => {
+  try {
+    const { type, price, name, code, description } = req.body;
+
+    const newApartment = new Apartment({
+      type,
+      price,
+      name,
+      code,
+      description
+    });
+
+    await newApartment.save();
+    res.status(201).json({ message: 'Apartment added successfully!' });
+  } catch (err) {
+    res.status(400).json({ error: 'Error: ' + err });
+  }
+});
+
 app.get('/contact',async(req,res)=>{
   try{
     const Employees= await Employee.find();
